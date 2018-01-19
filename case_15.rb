a37851c8327ad02bc729fd0f4f922c9378fd9b69
Alexander Rogalsky <alexander.rogalsky@yandex.ru>
2018-01-19 11:27:50 +0300

    Changeset to ruby sampels

--- case_15.rb
+++ case_15.rb
@@ -565,6 +565,35 @@ ObjectSpace.each_object(Class) {|c| puts c}
 # end
 # sleep(6)
 #----------------------------------------------------
+class Module
+	private
+
+	def readonly(*syms)
+		return if syms.size == 0
+		code = ""
+		syms.each do |s|
+			code << "def #{s}; @#{s}; end\n"
+		end
+		class_eval code
+	end
+
+	def readwrite(*syms)
+		return if syms.size == 0
+		code = ""
+		syms.each do |s|
+			code << "def #{s}; @#{s}; end\n"
+			code << "def #{s}=(value); @#{s} = value; end\n"
+		end
+		class_eval code
+
+	end
+end
+#----------------------------------------------------
+
+
+
+
+
 
 
 
